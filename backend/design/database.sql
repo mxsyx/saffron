6d1bd9f1365161a091e016a30f7224de6c37ba7d
zsimline <zsimline@163.com>
2019-11-11 10:45:38 +0800

    2028

--- backend/design/database.sql
+++ backend/design/database.sql
@@ -1,22 +1,20 @@
 -- 藏红花
-CREATE DATABASE saffron;
-USE saffron;
+CREATE DATABASE `saffron`;
+USE `saffron`;
 
--- 用户数据表
-CREATE TABLE user
+-- 用户信息表
+CREATE TABLE `user`
 (
-  id INT UNSIGNED AUTO_INCREMENT,       -- 用户ID
-  email VARCHAR254) NOT NULL,          -- 电子邮件作账号)
-  pwd CHAR44) NOT NULL,                -- 用户密码
-  nickname VARCHAR26) DEFAULT NULL,   -- 用户昵称
-  is_staff TINYINT1) DEFAULT 0,        -- 是否为管理人员
-  is_active TINYINT1) DEFAULT 0,       -- 账号是否可用
-  last_login DATETIME DEFAULT NULL,     -- 最后登录的时间
-  date_joined DATETIME NOT NULL,        -- 加入网站的时间
-  PRIMARY KEY `id`)
+  `id` INT UNSIGNED AUTO_INCREMENT,     -- 用户ID
+  `email` VARCHAR(254) NOT NULL,        -- 电子邮件(作账号)
+  `pwd` CHAR(44) NOT NULL,              -- 用户密码
+  `nickname` VARCHAR(26) DEFAULT NULL,  -- 用户昵称
+  `is_staff` TINYINT(1)  DEFAULT 0,     -- 是否为管理人员
+  `is_active` TINYINT(1) DEFAULT 0,     -- 账号是否可用
+  `last_login` DATETIME DEFAULT NULL,   -- 最后登录的时间
+  `date_joined` DATETIME NOT NULL,      -- 加入网站的时间
+  PRIMARY KEY (`id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4
-
--- 不会覆盖所有的可能
 
 -- 电影信息表
 CREATE TABLE `movie`
@@ -34,8 +32,9 @@ CREATE TABLE `movie`
   `volume` INT UNSIGNED DEFAULT 0,      -- 总播放量
   `love`   INT UNSIGNED DEFAULT 0,      -- 点赞数量
   `hate`   INT UNSIGNED DEFAULT 0,      -- 讨厌数量
+  `update` DATETIME NOT NULL,           -- 更新时间
   PRIMARY KEY(`id`),
-  UNIQUE KEY `NAME` (`NAME`)
+  UNIQUE KEY `name` (`name`),
 )ENGINE=InnoDB AUTO_INCREMENT=1000001 DEFAULT CHARSET=utf8mb4
 
 -- 电影播放地址表
@@ -55,7 +54,8 @@ CREATE TABLE `addrmovie`
   `dladdr4` VARCHAR(255) DEFAULT NULL,    -- 下载线路4
   `dladdr5` VARCHAR(255) DEFAULT NULL,    -- 下载线路5
   `dladdr6` VARCHAR(255) DEFAULT NULL,    -- 下载线路6
-  PRIMARY KEY (`vid`,`episode`)
+  PRIMARY KEY (`vid`,`episode`),
+  FOREIGN KEY (`vid`) REFERENCES `movie`(`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 
 -- 电视剧信息表
@@ -74,8 +74,9 @@ CREATE TABLE `tv`
   `volume` INT UNSIGNED DEFAULT 0,      -- 总播放量
   `love`   INT UNSIGNED DEFAULT 0,      -- 点赞数量
   `hate`   INT UNSIGNED DEFAULT 0,      -- 讨厌数量
-  PRIMARY KEY(id),
-  UNIQUE KEY `NAME` (`NAME`)
+  `update` DATETIME NOT NULL,           -- 更新时间
+  PRIMARY KEY(`id`),
+  UNIQUE KEY `name` (`name`)
 )ENGINE=InnoDB AUTO_INCREMENT=6000001 DEFAULT CHARSET=utf8mb4
 
 -- 电视剧播放地址表
@@ -95,44 +96,27 @@ CREATE TABLE `addrtv`
   `dladdr4` VARCHAR(255) DEFAULT NULL,    -- 下载线路4
   `dladdr5` VARCHAR(255) DEFAULT NULL,    -- 下载线路5
   `dladdr6` VARCHAR(255) DEFAULT NULL,    -- 下载线路6
-  PRIMARY KEY (`vid`,`episode`)
+  PRIMARY KEY (`vid`,`episode`),
+  FOREIGN KEY (`vid`) REFERENCES `tv`(`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 
 -- 历史记录表
-CREATE TABLE history
-(
-
-)
+CREATE TABLE `history` ()
 
 -- 收藏数据表
-CREATE TABLE collection
-(
-  
-)
+CREATE TABLE `collection`()
 
 -- 轮播数据表
-CREATE TABLE carousel
-(
-
-)
+CREATE TABLE `carousel`()
 
 -- 反馈信息表
-CREATE TABLE feedback
-(
-
-)
+CREATE TABLE `feedback`()
 
 -- 用户留言表
-CREATE TABLE comments
-(
-
-)
+CREATE TABLE `comments`()
 
 -- 系统消息表
-CREATE TABLE message
-(
-
-)
+CREATE TABLE `message`()
 
 -- 弹幕数据表
 CREATE TABLE `danmaku`
@@ -143,7 +127,7 @@ CREATE TABLE `danmaku`
   `text` VARCHAR(255) NOT NULL,         -- 弹幕内容
   `color` INT NOT NULL,                 -- 弹幕颜色
   `type` INT NOT NULL,                  -- 弹幕类型
-  `time` DECMICAL(5,0) NOT NULL,        -- 弹幕出现的时间
+  `time` DOUBLE NOT NULL,               -- 弹幕出现的时间
   `date` DATETIME NOT NULL,             -- 弹幕发送的时间
   `addr` CHAR(15) NOT NULL,             -- 弹幕发送者IP
   PRIMARY KEY (`_id`)

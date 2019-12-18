4870f70e969114fbc691011d3b51a754c2652abe
zsimline <zsimline@163.com>
2019-12-18 22:29:00 +0800

    2098

--- backend/service/config.mjs
+++ backend/service/config.mjs
@@ -5,7 +5,7 @@ const STATEMENTS = {
   'getDlAddr'   : "SELECT `addr` FROM  `dladdr` WHERE `vid`=? AND `episode`=?",
   'getAllPlAddr': 'SELECT `addr1`,`addr2`,`addr3`,`addr4`,`addr5`,`addr6` FROM `pladdr` WHERE vid=?',
   'getLatest'   : "(SELECT `id`,`name`,`imgaddr`,`actors` FROM `info` WHERE `bigtype`='mv' ORDER BY `update` DESC LIMIT 12) UNION (SELECT `id`,`name`,`imgaddr`,`actors` FROM `info` WHERE `bigtype`='tv' ORDER BY `update` DESC LIMIT 12)",
-  'getRandom'   : "SELECT `id`,`name`,'imgaddr',`actors` FROM `info` AS t1 JOIN (SELECT ROUND(RAND() * ((SELECT MAX(id) FROM `info`)-(SELECT MIN(id) FROM `info`))+(SELECT MIN(id) FROM `info`)) AS _id) AS t2 WHERE t1.id > t2._id LIMIT 12",
+  'getRandom'   : "SELECT `id`,`name`,`imgaddr`,`actors` FROM `info` AS t1 JOIN (SELECT ROUND(RAND() * ((SELECT MAX(id) FROM `info`)-(SELECT MIN(id) FROM `info`))+(SELECT MIN(id) FROM `info`)) AS _id) AS t2 WHERE t1.id > t2._id LIMIT 12",
   'getDanmaku'  : "SELECT `time`,`type`,`color`,`author`,`text` FROM danmaku WHERE `id`=? LIMIT ?",
   'addDanmaku'  : "INSERT INTO `danmaku` (`id`,`author`,`text`,`color`,`type`,`time`,`date`,`addr`) values (?,?,?,?,?,?,?,?)",
 }
--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -11,6 +11,10 @@
       headerTip="最新电视剧"
       v-bind:videoItems="tvItems"
     />
+    <DisplayBox
+      headerTip="随机推荐"
+      v-bind:videoItems="rdItems"
+    />
   </div>
 </template>
 
@@ -30,13 +34,16 @@ export default {
 
   mixins: [mixin],
 
-  beforeRouteEnter(to, from, next) {
-    axios.get('http://zizaixian.top/main/latest')
-      .then(response => {
-        next(vm => vm.setLatestVideoInfo(response.data));
-      })
-      .catch(error => {
-        this.$message('error', '加载网站数据失败')
+  created() {
+    axios.all([
+      axios.get('http://zizaixian.top/main/latest'),
+      axios.get('http://zizaixian.top/main/random'),
+    ])
+    .then(axios.spread((resLatest, resRandom) => {
+      this.setVideoData(resLatest.data, resRandom.data);
+    }))
+    .catch(error => {
+      this.$message('error', '加载网站数据失败')
     });
   },
 
@@ -48,17 +55,24 @@ export default {
     return {
       mvItems: null,
       tvItems: null,
+      rdItems: null,
       movieTypes: ['动作片','喜剧片','爱情片','科幻片','恐怖片','剧情片','战争片','动漫片','微电影'],
       tvTypes: ['国产剧','港台剧','日韩剧','欧美剧','动漫剧'],
     }
   },
 
   methods: {
-    setLatestVideoInfo(latestVideoInfo) {
-      this.mvItems = latestVideoInfo.slice(0, 12);
-      this.tvItems = latestVideoInfo.slice(12,24);
+    setVideoData(latestVideoData, randomVideoData) {
+      this.mvItems = latestVideoData.slice(0, 12);
+      this.tvItems = latestVideoData.slice(12,24);
+      this.rdItems = randomVideoData;
       this.$loaded();
-    }
+    },
+
+    setRandomVideoInfo(randomVideoInfo) {
+      console.log('xxx');
+      this.rdItems = randomVideoInfo;
+    },
   }
 }
 </script> 
